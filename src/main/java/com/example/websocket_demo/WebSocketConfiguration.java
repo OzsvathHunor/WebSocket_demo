a3839a3963a1f5c693f72dafdcf4afa9ee6c3ae9
OzsvathHunor <45239731+OzsvathHunor@users.noreply.github.com>
2023-03-09 11:15:27 +0100

    comments added

--- src/main/java/com/example/websocket_demo/WebSocketConfiguration.java
+++ src/main/java/com/example/websocket_demo/WebSocketConfiguration.java
@@ -11,16 +11,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-    @Override//letrehoz egy endpointot
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //letrehoz egy endpointot
         registry.addEndpoint("/stomp-endpoint")
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");//ez allitja be h a /topic utvonalra fel lehet iratkozni, itt kapja meg az uzeneteket
-        registry.setApplicationDestinationPrefixes("/app");//ez allitja be h milyen utvonal prefixel kezdodik ahova tudsz kuldeni
+        //ez allitja be h a /topic utvonalra fel lehet iratkozni, itt kapja meg az uzeneteket
+        registry.enableSimpleBroker("/topic");
+        //ez allitja be h milyen utvonal prefixel kezdodik ahova tudsz kuldeni
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
 }
